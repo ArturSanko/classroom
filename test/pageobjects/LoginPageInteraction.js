ea6cda1aae86a566b097b718d8acd6568f7162ef
Artur Sanko <asanko@exadel.com>
2022-07-14 10:54:10 +0200

    used prettier

--- test/pageobjects/LoginPageInteraction.js
+++ test/pageobjects/LoginPageInteraction.js
@@ -1,57 +1,57 @@
-const BaseInteraction = require('./BaseInteraction');
+const BaseInteraction = require("./BaseInteraction");
 
 class LoginPageInteraction extends BaseInteraction {
-    constructor(){
-        super()
-        this.url = 'https://automationteststore.com/';
+  constructor() {
+    super();
+    this.url = "https://automationteststore.com/";
 
-        this.buttonLoginOrRegister = '//*[text()="Login or register"]';
+    this.buttonLoginOrRegister = '//*[text()="Login or register"]';
 
-        this.loginNameField = '#loginFrm_loginname';
-        this.passwordField = '#loginFrm_password';
+    this.loginNameField = "#loginFrm_loginname";
+    this.passwordField = "#loginFrm_password";
 
-        this.buttonLogin = '//*[@title="Login"]';
+    this.buttonLogin = '//*[@title="Login"]';
 
-        this.userName = '//*[text()="Welcome back Name"]'
+    this.userName = '//*[text()="Welcome back Name"]';
 
-        this.errorMessage = '//div[starts-with(@class, "alert")]';
-            }
+    this.errorMessage = '//div[starts-with(@class, "alert")]';
+  }
 
-    async openURL(){
-        await this.openBrowserWithURL(this.url)
-    }
+  async openURL() {
+    await this.openBrowserWithURL(this.url);
+  }
 
-    async clickButtonLoginOrRegister(){
-        await this.waitForDisplayedAnElement(this.buttonLoginOrRegister);
-        await this.clickElement(this.buttonLoginOrRegister);
-    }
+  async clickButtonLoginOrRegister() {
+    await this.waitForDisplayedAnElement(this.buttonLoginOrRegister);
+    await this.clickElement(this.buttonLoginOrRegister);
+  }
 
-    async inputLogin(data){
-        await this.waitForDisplayedAnElement(this.loginNameField);
-        await this.inputTextIntoElement(this.loginNameField, data)
-    }
+  async inputLogin(data) {
+    await this.waitForDisplayedAnElement(this.loginNameField);
+    await this.inputTextIntoElement(this.loginNameField, data);
+  }
 
-    async inputPassword(data){
-        await this.waitForDisplayedAnElement(this.passwordField);
-        await this.inputTextIntoElement(this.passwordField, data)
-    }
+  async inputPassword(data) {
+    await this.waitForDisplayedAnElement(this.passwordField);
+    await this.inputTextIntoElement(this.passwordField, data);
+  }
 
-    async clickButtonLogin(){
-        await this.waitForDisplayedAnElement(this.buttonLogin);
-        await this.clickElement(this.buttonLogin)
-    }
+  async clickButtonLogin() {
+    await this.waitForDisplayedAnElement(this.buttonLogin);
+    await this.clickElement(this.buttonLogin);
+  }
 
-    async waitForDisappearLoginButton(){
-        await this.waitForDisappearAnElement(this.buttonLogin)
-    }
+  async waitForDisappearLoginButton() {
+    await this.waitForDisappearAnElement(this.buttonLogin);
+  }
 
-    async waitForDisplayedUserName(){
-        await this.waitForDisplayedAnElement(this.userName)
-    }
+  async waitForDisplayedUserName() {
+    await this.waitForDisplayedAnElement(this.userName);
+  }
 
-    async waitForDisplayedErrorMessage(){
-        await this.waitForDisplayedAnElement(this.errorMessage)
-    }
+  async waitForDisplayedErrorMessage() {
+    await this.waitForDisplayedAnElement(this.errorMessage);
+  }
 }
 
 module.exports = new LoginPageInteraction();
--- test/specs/LoginSmokeTestPositive.js
+++ test/specs/LoginSmokeTestPositive.js
@@ -1,17 +1,17 @@
-const loginCredentials = require('../models/LoginCredentials');
-const loginPageInteraction = require('../pageobjects/LoginPageInteraction');
+const loginCredentials = require("../models/LoginCredentials");
+const loginPageInteraction = require("../pageobjects/LoginPageInteraction");
 
-describe('Smoke positive test for log in', function(){
-    before(async function(){
-        await loginPageInteraction.openURL();
-        await loginPageInteraction.clickButtonLoginOrRegister();
-        await loginPageInteraction.inputLogin(loginCredentials.login);
-        await loginPageInteraction.inputPassword(loginCredentials.validPassword);
-        await loginPageInteraction.clickButtonLogin();
-        await loginPageInteraction.waitForDisappearLoginButton();
-    })
+describe("Smoke positive test for log in", function () {
+  before(async function () {
+    await loginPageInteraction.openURL();
+    await loginPageInteraction.clickButtonLoginOrRegister();
+    await loginPageInteraction.inputLogin(loginCredentials.login);
+    await loginPageInteraction.inputPassword(loginCredentials.validPassword);
+    await loginPageInteraction.clickButtonLogin();
+    await loginPageInteraction.waitForDisappearLoginButton();
+  });
 
-    it('user have to log in', async function(){
-        await loginPageInteraction.waitForDisplayedUserName();
-    });
+  it("user have to log in", async function () {
+    await loginPageInteraction.waitForDisplayedUserName();
+  });
 });
