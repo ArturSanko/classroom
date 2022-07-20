eb48b6bdb48adb07fc82f3584c98bd6185fe5df0
Artur Sanko <asanko@exadel.com>
2022-07-20 22:28:39 +0200

    redo login negative test

--- test/specs/LoginSmokeTestNegative.js
+++ test/specs/LoginSmokeTestNegative.js
@@ -5,12 +5,25 @@ describe('Smoke negative test for log in', function () {
   before(async function () {
     await loginPageInteraction.openURL();
     await loginPageInteraction.clickButtonLoginOrRegister();
+  });
+
+  it('account login page is opened', async function () {
+    await loginPageInteraction.waitForDisplayedAccountLoginPage();
+  });
+
+  it('credentials are invalid', async function () {
     await loginPageInteraction.inputLogin(loginCredentials.login);
     await loginPageInteraction.inputPassword(loginCredentials.invalidPassword);
+    // await expect(loginPageInteraction.getLoginText()).toHaveText(
+    //   loginCredentials.login
+    // );
+    // await expect(loginPageInteraction.getPasswordText()).toHaveText(
+    //   loginCredentials.invalidPassword
+    // );
     await loginPageInteraction.clickButtonLogin();
   });
 
-  it('user have to get error message', async function () {
+  it('user have to get error notification', async function () {
     await loginPageInteraction.waitForDisplayedErrorMessage();
   });
 });
