a6ad6ca1ac46bb816103f88c918cbbbfb6fcf8ab
Artur Sanko <asanko@exadel.com>
2022-09-08 16:29:59 +0200

    done first api test

--- tests/pet.test.js
+++ tests/pet.test.js
@@ -1,14 +1,17 @@
-var { got } = require('got');
-var { assert } = require('assert').strict;
+// var { got } = require('../node_modules/got');
+// import got from '../node_modules/got';
+import got from 'got';
+import { strict as assert } from 'assert';
+// var { assert } = require('assert').strict;
 
 describe('Pet', async () => {
   it("Get pet's ID", async () => {
-    const response = await got('https://petstore.swagger.io/pet/1');
+    const response = await got('https://petstore.swagger.io/v2/pet/3');
     const body = JSON.parse(response.body);
 
     assert(
-      body.id === 1,
-      `Expacted API to return pet with ID 1, but got ${body.id}`
+      body.id === 3,
+      `Expacted API to return pet with ID 3, but got ${body.id}`
     );
   });
 });
