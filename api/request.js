c96d564fbd891cc25fdb329fad451ab1c258bd0f
Artur Sanko <asanko@exadel.com>
2022-09-10 19:51:17 +0200

    undo task from 3 video

--- api/request.js
+++ api/request.js
@@ -1,3 +1,32 @@
-export class JSONRequest{
-    
+import { Options, Method } from 'got';
+import got from 'got';
+
+export class JSONRequest {
+  //   protected options: any = {
+  //      responsTyoe: 'json';
+  //   }
+
+  url(url) {
+    this.options.url = url;
+    return this;
+  }
+
+  method(method) {
+    this.options.method = method;
+    return this;
+  }
+
+  searchParams(searchParams) {
+    this.options.searchParams = searchParams;
+    return this;
+  }
+
+  body(body) {
+    this.options.json = body;
+    return this;
+  }
+
+  send() {
+    return got(this.options);
+  }
 }
