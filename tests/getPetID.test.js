cf80fb746cabe5d9d1f1e723ccb3b1040b88551e
Artur Sanko <asanko@exadel.com>
2022-09-16 13:21:34 +0200

    users id test

--- tests/getPetID.test.js
+++ tests/getPetID.test.js
@@ -1,16 +1,14 @@
 import supertest from 'supertest';
 import { expect } from 'chai';
-// const request = supertest('https://gorest.co.in/public/v2/');
 const request = supertest('https://petstore.swagger.io/v2/');
 
-describe('Pets', async () => {
-  it("Get pet's id", async () => {
-    //done
-    await request.get('pet/0').end((err, res) => {
+describe('Pets', () => {
+  it("Get pet's id", (done) => {
+    request.get('pet/3').end((err, res) => {
       console.log(err);
       console.log(res.body);
-      expect(res.body.id).to.equal(1);
-      //done()
+      expect(res.body.id).to.equal(3);
+      done();
     });
   });
 });
